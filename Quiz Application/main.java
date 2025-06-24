9cb1f908064b9132b3f8b02dff549c0ea52baf6f
Abonti Jannati <154469170+abon-teaa@users.noreply.github.com>
2025-06-25 01:16:23 +0600

    Delete Quiz Application directory

--- Quiz Application/main.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-import java.io.*;
-import quiz.application.*;
-
- public static void main(String[] args) {
-      new Login();
-   }
